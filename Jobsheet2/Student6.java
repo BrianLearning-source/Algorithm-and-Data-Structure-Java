9c041923cd970679793bb1f16a3e6ca3ed7e05b7
BrianSerafino <briandonovan696@gmail.com>
2025-02-26 02:28:22 +0700

    Add files via upload

---
+++ Jobsheet2/Student6.java
@@ -0,0 +1,34 @@
+public class Student6 {
+    
+    String studentID;
+    String name;
+    String className;
+    double gpa;
+
+    void print() {
+        System.out.println("Student ID : " + studentID);
+        System.out.println("Name : " + name);
+        System.out.println("Class : " + className);
+        System.out.println("GPA : " + gpa);
+    }
+    void changeClass(String newClass) {
+        className = newClass;
+    }
+    void updateGPA(double newGPA) {
+        gpa = newGPA;
+        if (newGPA <= 0.0 || newGPA >= 4.0) {
+            System.out.println("Invalid IPK. Must be between 0.0 and 4.0.");
+        }
+    }
+    String evaluate() {
+        if(gpa >= 3.5) {
+            return "Excellent";
+        } else if(gpa >= 3.0) {
+            return "Good";
+        } else if(gpa >=2.0) {
+            return "Fair";
+        } else {
+            return "Poor";
+        }
+    }
+}
