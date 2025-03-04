48ddc767bc021505ced23d842c241a21d0c0062f
BrianSerafino <briandonovan696@gmail.com>
2025-03-05 05:14:38 +0700

    Update Triangle.java

--- Jobsheet3/week3/Triangle.java
+++ Jobsheet3/week3/Triangle.java
@@ -3,4 +3,18 @@ package week3;
 public class Triangle {
     public int base;
     public int height;
+
+public Triangle (int a, int t) {
+    base = a;
+    height = t;
+}
+
+double countArea() {
+ return 0.5 * base * height;
+}
+
+double countPerimeter() {
+    double hypotenusa = Math.sqrt(((0.5 * base) * (0.5 * base) + height * height));
+ return 2 * hypotenusa + base;
+}
 }
