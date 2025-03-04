1c61ac28b7a354b3311f97db89a9caa230cd2fa5
BrianSerafino <briandonovan696@gmail.com>
2025-03-05 05:24:18 +0700

    Add files via upload

---
+++ Jobsheet3/week3/ArrayOfObject2.java
@@ -0,0 +1,33 @@
+package week3;
+
+import java.util.Scanner;
+
+public class ArrayOfObject2 {
+public static void main(String[] args) {
+            
+            Scanner sc = new Scanner(System.in);
+            System.out.print("Input the length of array of object : " );
+            int i = sc.nextInt();
+            Rectangle[] rectangleArray = new Rectangle[i];
+            // Assign the values for each attributes in objects
+
+            for (int j = 0; j < rectangleArray.length; j++) {
+               rectangleArray[j] = new Rectangle();
+                System.out.println("Rectangle " + j);
+
+                System.out.print("Input length : ");
+                rectangleArray[j].length = sc.nextInt();
+
+                System.out.print("Input width : ");
+                rectangleArray[j].width = sc.nextInt();
+            }
+
+            // Display the result in console
+            for (int k = 0; k < 10; k++) {
+                System.out.println("Rectangle " + k);
+                System.out.println("Width : " + rectangleArray[0].width + ", length : " + rectangleArray[0].length);
+            }
+
+        }
+    
+}
